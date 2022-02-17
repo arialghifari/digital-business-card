c3c91a37e6aac34718f17432926a4390303f2307
arialghifari <arialghifari99@gmail.com>
2022-02-18 00:00:26 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import "./main.css";
+
+const App = () => {
+    return (
+        <main>
+            <h1>Hai there!</h1>
+        </main>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
